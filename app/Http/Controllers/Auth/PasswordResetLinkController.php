6de3cb58b0581b751274b0399db3aff55058d564
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2022-08-30 02:14:46 +0500

    # Email testing

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -39,7 +39,7 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        try{
+        try {
             $status = Password::sendResetLink(
                 $request->only('email')
             );
@@ -47,17 +47,15 @@ class PasswordResetLinkController extends Controller
             if ($status == Password::RESET_LINK_SENT) {
                 return back()->with('status', __($status));
             }
-    
+
             throw ValidationException::withMessages([
                 'email' => [trans($status)],
             ]);
-    
-        }catch(\Throwable $e){
+        } catch (\Throwable $e) {
+            dd($e);
             throw ValidationException::withMessages([
                 'error' => 'Something went wrong',
             ]);
         }
-
-        
     }
 }
