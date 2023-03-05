c5d3bf33890ac01a1f3607cae03b516a612c52e7
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-03-05 16:34:37 +0500

    #login_page_changes

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -1,39 +1,54 @@
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <!-- <inertia-link href="/">
+	<div
+		class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+	>
+		<div>
+			<!-- <inertia-link href="/">
                 <img alt="shippingxps" width="237" height="55" src="/theme/img/logo.png">
             </inertia-link> -->
-            <a href="http://shippingxps.com">
-                <img alt="shippingxps" width="237" height="55" src="/theme/img/logo.png">
-            </a>
-        </div>
+			<a href="http://shippingxps.com">
+				<img
+					alt="shippingxps"
+					width="237"
+					height="55"
+					src="/theme/img/logo.png"
+				/>
+			</a>
+		</div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <slot />
-        </div>
-    </div>
+		<div
+			class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+		>
+			<slot />
+		</div>
+
+		<div>
+			<PricingContentComponent />
+		</div>
+	</div>
 </template>
 
 <script>
+	var Tawk_API = Tawk_API || {},
+		Tawk_LoadStart = new Date();
+	(function () {
+		console.log("Tawk To Loaded");
+		var s1 = document.createElement("script"),
+			s0 = document.getElementsByTagName("script")[0];
+		s1.async = true;
+		s1.src = "https://embed.tawk.to/617d793f86aee40a573920c6/1fj9238e6";
+		s1.charset = "UTF-8";
+		s1.setAttribute("crossorigin", "*");
+		s0.parentNode.insertBefore(s1, s0);
+	})();
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-  console.log('Tawk To Loaded')
-  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  s1.async=true;
-  s1.src='https://embed.tawk.to/617d793f86aee40a573920c6/1fj9238e6';
-  s1.charset='UTF-8';
-  s1.setAttribute('crossorigin','*');
-  s0.parentNode.insertBefore(s1,s0);
-})();
+	import BreezeApplicationLogo from "@/Components/ApplicationLogo";
+	import PricingContentComponent from "../Components/PricingContent.vue";
 
-
-import BreezeApplicationLogo from '@/Components/ApplicationLogo'
-
-    export default {
-        components: {
-            BreezeApplicationLogo,
-        }
-    }
+	export default {
+		components: {
+			BreezeApplicationLogo,
+			PricingContentComponent,
+		},
+	};
 </script>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,117 +1,168 @@
 <template>
-    <breeze-validation-errors class="mb-4" />
+	<breeze-validation-errors class="mb-4" />
 
-    <form @submit.prevent="submit">
-        <div>
-            <breeze-label for="name" value="Name" />
-            <breeze-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
-        </div>
+	<form @submit.prevent="submit">
+		<div>
+			<breeze-label for="name" value="Name" />
+			<breeze-input
+				id="name"
+				type="text"
+				class="mt-1 block w-full"
+				v-model="form.name"
+				required
+				autofocus
+				autocomplete="name"
+			/>
+		</div>
 
-        <div class="mt-4">
-            <breeze-label for="email" value="Email" />
-            <breeze-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autocomplete="username" />
-        </div>
+		<div class="mt-4">
+			<breeze-label for="email" value="Email" />
+			<breeze-input
+				id="email"
+				type="email"
+				class="mt-1 block w-full"
+				v-model="form.email"
+				required
+				autocomplete="username"
+			/>
+		</div>
 
-        <div class="mt-4">
-            <breeze-label for="email" value="Phone" />
-            <!-- <breeze-input id="phone_no" type="text" class="mt-1 block w-full" v-model="form.phone_no" required autocomplete="phone_no" /> -->
-            <vue-tel-input v-model="form.phone_no" mode="international" class="mt-1 block w-full" required default-country="US" :valid-characters-only="true"></vue-tel-input>
-        </div>
+		<div class="mt-4">
+			<breeze-label for="email" value="Phone" />
+			<vue-tel-input
+				v-model="form.phone_no"
+				mode="international"
+				class="mt-1 block w-full"
+				required
+				default-country="US"
+				:valid-characters-only="true"
+			></vue-tel-input>
+		</div>
 
-        <div class="mt-4">
-            <breeze-label for="password" value="Password" />
-            <breeze-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
-        </div>
+		<div class="mt-4">
+			<breeze-label for="password" value="Password" />
+			<breeze-input
+				id="password"
+				type="password"
+				class="mt-1 block w-full"
+				v-model="form.password"
+				required
+				autocomplete="new-password"
+			/>
+		</div>
 
-        <div class="mt-4">
-            <breeze-label for="password_confirmation" value="Confirm Password" />
-            <breeze-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
-        </div>
+		<div class="mt-4">
+			<breeze-label for="password_confirmation" value="Confirm Password" />
+			<breeze-input
+				id="password_confirmation"
+				type="password"
+				class="mt-1 block w-full"
+				v-model="form.password_confirmation"
+				required
+				autocomplete="new-password"
+			/>
+		</div>
 
-        <div class="form-group col-md-12 mt-4">
-            <breeze-label for="hear_from" value="How did you hear about us?" />
-            <select class="form-control mt-1 block w-full" name="type" v-model="form.hear_from" id="type" required>
-                <option value="" selected>--Please Select--</option>
-                <option value="Google">Google</option>
-                <option value="Instagram">Instagram</option>
-                <option value="Twitter">Twitter</option>
-                <option value="Youtube">Youtube</option>
-                <option value="Articles & Blogs">Articles & Blogs</option>
-                <option value="Friend Recommendation">Friend Recommendation</option>
-                <option value="Other">Other</option>
-            </select>
-        </div>
+		<div class="form-group col-md-12 mt-4">
+			<breeze-label for="hear_from" value="How did you hear about us?" />
+			<select
+				class="form-control mt-1 block w-full"
+				name="type"
+				v-model="form.hear_from"
+				id="type"
+				required
+			>
+				<option value="" selected>--Please Select--</option>
+				<option value="Google">Google</option>
+				<option value="Instagram">Instagram</option>
+				<option value="Twitter">Twitter</option>
+				<option value="Youtube">Youtube</option>
+				<option value="Articles & Blogs">Articles & Blogs</option>
+				<option value="Friend Recommendation">Friend Recommendation</option>
+				<option value="Other">Other</option>
+			</select>
+		</div>
 
-        <div class="flex items-center justify-end mt-4">
-            <inertia-link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                Already registered?
-            </inertia-link>
+		<div class="flex items-center justify-end mt-4">
+			<inertia-link
+				:href="route('login')"
+				class="underline text-sm text-gray-600 hover:text-gray-900"
+			>
+				Already registered? Login now
+			</inertia-link>
 
-            <breeze-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Register
-            </breeze-button>
-        </div>
-    </form>
+			<breeze-button
+				class="ml-4"
+				:class="{ 'opacity-25': form.processing }"
+				:disabled="form.processing"
+			>
+				Register
+			</breeze-button>
+		</div>
+	</form>
 </template>
 
 <script>
-    import BreezeButton from '@/Components/Button'
-    import BreezeGuestLayout from '@/Layouts/Guest'
-    import BreezeInput from '@/Components/Input'
-    import BreezeLabel from '@/Components/Label'
-    import BreezeValidationErrors from '@/Components/ValidationErrors'
-    import { VueTelInput } from 'vue-tel-input';
-    import 'vue-tel-input/dist/vue-tel-input.css';
+	import BreezeButton from "@/Components/Button";
+	import BreezeGuestLayout from "@/Layouts/Guest";
+	import BreezeInput from "@/Components/Input";
+	import BreezeLabel from "@/Components/Label";
+	import BreezeValidationErrors from "@/Components/ValidationErrors";
+	import { VueTelInput } from "vue-tel-input";
+	import "vue-tel-input/dist/vue-tel-input.css";
 
-    export default {
-        layout: BreezeGuestLayout,
+	export default {
+		layout: BreezeGuestLayout,
 
-        components: {
-            BreezeButton,
-            BreezeInput,
-            BreezeLabel,
-            BreezeValidationErrors,
-            VueTelInput,
-        },
+		components: {
+			BreezeButton,
+			BreezeInput,
+			BreezeLabel,
+			BreezeValidationErrors,
+			VueTelInput,
+		},
 
-        props: {
-            auth: Object,
-            errors: Object,
-        },
+		props: {
+			auth: Object,
+			errors: Object,
+		},
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    name: '',
-                    email: '',
-                    phone_no: '',
-                    password: '',
-                    password_confirmation: '',
-                    hear_from: '',
-                    captcha_token: "",
-                })
-            }
-        },
+		data() {
+			return {
+				form: this.$inertia.form({
+					name: "",
+					email: "",
+					phone_no: "",
+					password: "",
+					password_confirmation: "",
+					hear_from: "",
+					captcha_token: "",
+				}),
+			};
+		},
 
-        methods: {
-            submit() {
-                // this.form.post(this.route('register'), {
-                //     onFinish: () => this.form.reset('password', 'password_confirmation'),
-                // })
-                
-                let submitForm = (token) => {
-                    this.form.captcha_token = token;
-                    this.form.post(this.route("register"), {
-                        onFinish: () => this.form.reset("password", "password_confirmation"),
-                    });
-                };
+		methods: {
+			submit() {
+				// this.form.post(this.route('register'), {
+				//     onFinish: () => this.form.reset('password', 'password_confirmation'),
+				// })
 
-                grecaptcha
-                    .execute('6LcKxb0hAAAAALPcMiT1eLu03DnQfxaluzJhgD8F', { action: "submit" })
-                    .then(function (token) {
-                        submitForm(token);
-                    });
-            }
-        }
-    }
+				let submitForm = (token) => {
+					this.form.captcha_token = token;
+					this.form.post(this.route("register"), {
+						onFinish: () =>
+							this.form.reset("password", "password_confirmation"),
+					});
+				};
+
+				grecaptcha
+					.execute("6LcKxb0hAAAAALPcMiT1eLu03DnQfxaluzJhgD8F", {
+						action: "submit",
+					})
+					.then(function (token) {
+						submitForm(token);
+					});
+			},
+		},
+	};
 </script>
