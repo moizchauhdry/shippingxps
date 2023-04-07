ad291729ed33c74fe35bdfbb8c9dc56021fef09b
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-04-07 05:41:25 +0500

    #production_build

--- resources/js/Pages/Pricing.vue
+++ resources/js/Pages/Pricing.vue
@@ -259,7 +259,7 @@
 					<div class="d-flex justify-content-center">
 						<a href="http://shippingxps.com">
 							<img
-								class="mt-5"
+								class="mt-2"
 								alt="shippingxps"
 								width="237"
 								height="55"
