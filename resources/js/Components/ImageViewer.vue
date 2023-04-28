77df11450a369b7fb5aa652ca75f5c76c55f9bb6
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-04-29 04:15:19 +0500

    Merge branch 'staging'

--- resources/js/Components/ImageViewer.vue
+++ resources/js/Components/ImageViewer.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <div class="modal fade" id="imageViewer" tabindex="-1" role="dialog" aria-labelledby="imageViewer" aria-hidden="true">
+	<!-- <div class="modal fade" id="imageViewer" tabindex="-1" role="dialog" aria-labelledby="imageViewer" aria-hidden="true">
     <div class="modal-dialog modal-dialog-scrollable" role="document">
       <div class="modal-content">
         <button @click="closeImageViewer()" type="button" class="close" data-dismiss="modal" aria-label="Close" style="position:absolute;top:0 px;z-index:999;right:0;padding:0 px 10 px;background:#000;color:#fff;font-size:20px">
@@ -10,44 +10,58 @@
     </div>
   </div> -->
 
-<div class="modal fade" id="imageViewer" tabindex="-1" aria-labelledby="imageViewerLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable modal-xl">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="imageViewerLabel">Image Viewer</h5>
-        <button @click="closeImageViewer()" type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-          <img style="max-height: 100%;width:auto" src="">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeImageViewer()">Close</button>
-        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-      </div>
-    </div>
-  </div>
-</div>
-
-
+	<div
+		class="modal fade"
+		id="imageViewer"
+		tabindex="-1"
+		aria-labelledby="imageViewerLabel"
+		aria-hidden="true"
+	>
+		<div class="modal-dialog modal-dialog-scrollable modal-xl">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="imageViewerLabel">Image Viewer</h5>
+					<button
+						@click="closeImageViewer()"
+						type="button"
+						class="close"
+						data-dismiss="modal"
+						aria-label="Close"
+					>
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<img src="" alt="" />
+				</div>
+				<div class="modal-footer">
+					<button
+						type="button"
+						class="btn btn-secondary"
+						data-dismiss="modal"
+						@click="closeImageViewer()"
+					>
+						Close
+					</button>
+					<!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "ImageViewer",
-  methods:{
-    closeImageViewer(){
-      var modal = document.getElementById('imageViewer');
-      var imageSRC = document.querySelector('#imageViewer modal-body img');
-      modal.style.display = 'none';
-      imageSRC.src = "";
-    }
-
-  }
-}
+	export default {
+		name: "ImageViewer",
+		methods: {
+			closeImageViewer() {
+				var modal = document.getElementById("imageViewer");
+				var imageSRC = document.querySelector("#imageViewer modal-body img");
+				modal.style.display = "none";
+				imageSRC.src = "";
+			},
+		},
+	};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
