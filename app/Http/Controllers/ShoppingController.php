7ca2bb153aab39f60e9ac9b1bf6753548c06b442
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-11-19 23:47:04 +0500

    xps self service tasks

--- app/Http/Controllers/ShoppingController.php
+++ app/Http/Controllers/ShoppingController.php
@@ -7,20 +7,16 @@ use \Illuminate\Http\Response;
 use Inertia\Inertia;
 use GuzzleHttp\Client;
 use App\Models\City;
-use Exception;
+use App\Models\SiteSetting;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
 
-
   public function pricing()
   {
-
     $cities_from = City::where('country_code_2', 'US')->get()->toArray();
-
     $cities_to = City::where('country_code_2', '!=', 'US')->get()->toArray();
-
 
     return Inertia::render('Pricing', [
       'cities_from' => $cities_from,
@@ -31,8 +27,6 @@ class HomeController extends Controller
 
   public function shopping()
   {
-
-
     $user = Auth::user();
 
     $profile_complete = TRUE;
@@ -63,187 +57,186 @@ class HomeController extends Controller
   }
 
 
-  public function getListings()
-  {
+  // public function getListings()
+  // {
 
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://xpsshipper.com/restapi/v1/customers/12339140/services',
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => '',
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => 'GET',
-      CURLOPT_HTTPHEADER => array(
-        'Authorization: Bearer m99PyQIqoq5GmAKjs1wOTAbhQ0ozkc0s',
-        'Cookie: PHPSESSID=3rf796ooctiic30gq0e54bpg45'
-      ),
-    ));
+  //   $curl = curl_init();
+  //   curl_setopt_array($curl, array(
+  //     CURLOPT_URL => 'https://xpsshipper.com/restapi/v1/customers/12339140/services',
+  //     CURLOPT_RETURNTRANSFER => true,
+  //     CURLOPT_ENCODING => '',
+  //     CURLOPT_MAXREDIRS => 10,
+  //     CURLOPT_TIMEOUT => 0,
+  //     CURLOPT_FOLLOWLOCATION => true,
+  //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  //     CURLOPT_CUSTOMREQUEST => 'GET',
+  //     CURLOPT_HTTPHEADER => array(
+  //       'Authorization: Bearer m99PyQIqoq5GmAKjs1wOTAbhQ0ozkc0s',
+  //       'Cookie: PHPSESSID=3rf796ooctiic30gq0e54bpg45'
+  //     ),
+  //   ));
 
-    $response = curl_exec($curl);
+  //   $response = curl_exec($curl);
 
-    curl_close($curl);
-    $response = json_decode($response);
-  }
+  //   curl_close($curl);
+  //   $response = json_decode($response);
+  // }
 
   /*
     This will get quote for all selected packages and items.
 
     */
-  public function getQuotes(Request $request)
-  {
+  // public function getQuotes(Request $request)
+  // {
 
-    $markup = SiteSetting::getByName('markup');
+  //   $markup = SiteSetting::getByName('markup');
 
-    $ship_from_zip = $request->input('ship_from');
-    $ship_to_zip = $request->input('ship_to');
-    $weight = $request->input('weight');
-    $units = $request->input('weight_unit', 'lb_in');
-    $length = $request->input('length');
-    $width = $request->input('width');
-    $height = $request->input('height');
-    $declared_value = $request->input('declared_value');
-    $ship_from_city = City::where('zip', $ship_from_zip)->first();
-    $ship_to_city = City::where('zip', $ship_to_zip)->first();
+  //   $ship_from_zip = $request->input('ship_from');
+  //   $ship_to_zip = $request->input('ship_to');
+  //   $weight = $request->input('weight');
+  //   $units = $request->input('weight_unit', 'lb_in');
+  //   $length = $request->input('length');
+  //   $width = $request->input('width');
+  //   $height = $request->input('height');
+  //   $declared_value = $request->input('declared_value');
+  //   $ship_from_city = City::where('zip', $ship_from_zip)->first();
+  //   $ship_to_city = City::where('zip', $ship_to_zip)->first();
 
-    $units = explode('_', $units);
+  //   $units = explode('_', $units);
 
-    $weight_unit = isset($units[0]) ? $units[0] : 'lb';
-    $dimention_unit = isset($units[1]) ? $units[1] : 'in';
+  //   $weight_unit = isset($units[0]) ? $units[0] : 'lb';
+  //   $dimention_unit = isset($units[1]) ? $units[1] : 'in';
 
-    $headers = [
-      'cache-control' => 'no-cache',
-      'Content-Type' => 'application/json',
-      'Authorization' => 'Bearer ' . $this->token
-    ];
+  //   $headers = [
+  //     'cache-control' => 'no-cache',
+  //     'Content-Type' => 'application/json',
+  //     'Authorization' => 'Bearer ' . $this->token
+  //   ];
 
-    $client = new Client();
+  //   $client = new Client();
 
-    $request = $client->get('https://xpsshipper.com/restapi/v1/customers/12339140/services', [
-      'headers' => $headers,
-      'http_errors' => true,
-    ]);
+  //   $request = $client->get('https://xpsshipper.com/restapi/v1/customers/12339140/services', [
+  //     'headers' => $headers,
+  //     'http_errors' => true,
+  //   ]);
 
-    $response = $request ? $request->getBody()->getContents() : null;
+  //   $response = $request ? $request->getBody()->getContents() : null;
 
-    $response = json_decode($response);
+  //   $response = json_decode($response);
 
-    $service_rates = [];
+  //   $service_rates = [];
 
-    try {
+  //   try {
 
-      foreach ($response->services as $service) {
+  //     foreach ($response->services as $service) {
 
-        if (!empty($service->carrierCode) && strpos($service->serviceCode, 'international') !== false) {
+  //       if (!empty($service->carrierCode) && strpos($service->serviceCode, 'international') !== false) {
 
-          //echo "<br>Getting Rates for ".$service->serviceCode."<br>";
+  //         //echo "<br>Getting Rates for ".$service->serviceCode."<br>";
 
-          $sender = [
-            "country" =>  $ship_from_city->country_code_2,
-            "zip" =>  $ship_from_zip
-          ];
+  //         $sender = [
+  //           "country" =>  $ship_from_city->country_code_2,
+  //           "zip" =>  $ship_from_zip
+  //         ];
 
-          $receiver = [
-            "country" => $ship_to_city->country_code_2,
-            "zip" => $ship_to_zip
-          ];
+  //         $receiver = [
+  //           "country" => $ship_to_city->country_code_2,
+  //           "zip" => $ship_to_zip
+  //         ];
 
-          //$declared_value = "10";
+  //         //$declared_value = "10";
 
-          $pieces = [
-            0 => [
-              "weight" => $weight,
-              "length" => $length,
-              "width" => $width,
-              "height" => $height,
-              "insuranceAmount" => null,
-              "declaredValue" => $declared_value
-            ]
-          ];
+  //         $pieces = [
+  //           0 => [
+  //             "weight" => $weight,
+  //             "length" => $length,
+  //             "width" => $width,
+  //             "height" => $height,
+  //             "insuranceAmount" => null,
+  //             "declaredValue" => $declared_value
+  //           ]
+  //         ];
 
-          $post_params = [
-            "carrierCode" => $service->carrierCode,
-            "serviceCode" =>  $service->serviceCode,
-            "packageTypeCode" => $this->getPackageTypeCode($service),
-            "sender" => $sender,
-            "receiver" => $receiver,
-            "residential" => true,
-            "signatureOptionCode" => "DIRECT",
-            "contentDescription" => "stuff and things",
-            "weightUnit" => $weight_unit,
-            "dimUnit" => $dimention_unit,
-            "currency" => "USD",
-            "customsCurrency" => "USD",
-            "pieces"  => $pieces,
-            "billing" => [
-              "party" => "receiver"
-            ]
-          ];
+  //         $post_params = [
+  //           "carrierCode" => $service->carrierCode,
+  //           "serviceCode" =>  $service->serviceCode,
+  //           "packageTypeCode" => $this->getPackageTypeCode($service),
+  //           "sender" => $sender,
+  //           "receiver" => $receiver,
+  //           "residential" => true,
+  //           "signatureOptionCode" => "DIRECT",
+  //           "contentDescription" => "stuff and things",
+  //           "weightUnit" => $weight_unit,
+  //           "dimUnit" => $dimention_unit,
+  //           "currency" => "USD",
+  //           "customsCurrency" => "USD",
+  //           "pieces"  => $pieces,
+  //           "billing" => [
+  //             "party" => "receiver"
+  //           ]
+  //         ];
 
-          $request = $client->post('https://xpsshipper.com/restapi/v1/customers/' . $this->customer_id . '/quote', [
-            'headers' => $headers,
-            'body' => json_encode($post_params),
-            'http_errors' => true,
-          ]);
+  //         $request = $client->post('https://xpsshipper.com/restapi/v1/customers/' . $this->customer_id . '/quote', [
+  //           'headers' => $headers,
+  //           'body' => json_encode($post_params),
+  //           'http_errors' => true,
+  //         ]);
 
-          $response = $request ? $request->getBody()->getContents() : null;
+  //         $response = $request ? $request->getBody()->getContents() : null;
 
-          $response = json_decode($response);
+  //         $response = json_decode($response);
 
-          $markup_amount = $response->totalAmount * ((int)$markup / 100);
+  //         $markup_amount = $response->totalAmount * ((int)$markup / 100);
 
-          $total = $response->totalAmount + $markup_amount;
+  //         $total = $response->totalAmount + $markup_amount;
 
-          $service_rates[] = [
-            'label' => $service->serviceLabel,
-            'serviceCode' => $service->serviceCode,
-            'currency' => $response->currency,
-            //'totalAmount' => $response->totalAmount,
-            'totalAmount' => $total,
-            'baseAmount' => $response->baseAmount,
-          ];
-        }
-      }
+  //         $service_rates[] = [
+  //           'label' => $service->serviceLabel,
+  //           'serviceCode' => $service->serviceCode,
+  //           'currency' => $response->currency,
+  //           //'totalAmount' => $response->totalAmount,
+  //           'totalAmount' => $total,
+  //           'baseAmount' => $response->baseAmount,
+  //         ];
+  //       }
+  //     }
 
-      return response()->json(['status' => TRUE, 'services' => $service_rates]);
-    } catch (\Exception $ex) {
+  //     return response()->json(['status' => TRUE, 'services' => $service_rates]);
+  //   } catch (\Exception $ex) {
 
-      $ex_message = $ex->getMessage();
+  //     $ex_message = $ex->getMessage();
 
-      $pos = strpos($ex_message, '{"error":"');
+  //     $pos = strpos($ex_message, '{"error":"');
 
-      $pos1 = strpos($ex_message, '"errorCategory"');
-      $length = $pos1 - ($pos + 12);
+  //     $pos1 = strpos($ex_message, '"errorCategory"');
+  //     $length = $pos1 - ($pos + 12);
 
-      $message = substr($ex_message, $pos + 10, $length);
+  //     $message = substr($ex_message, $pos + 10, $length);
 
-      return response()->json([
-        'status' => FALSE,
-        'message' => $message,
-      ]);
-    }
-  }
+  //     return response()->json([
+  //       'status' => FALSE,
+  //       'message' => $message,
+  //     ]);
+  //   }
+  // }
 
+  // private function getPackageTypeCode($service)
+  // {
 
-  private function getPackageTypeCode($service)
-  {
+  //   $type = "";
 
-    $type = "";
+  //   switch ($service->carrierCode) {
+  //     case "dhl":
+  //       $type =  "dhl_custom_package";
+  //       break;
+  //     case "fedex":
+  //       $type = "fedex_custom_package";
+  //       break;
+  //     case "usps":
+  //       $type = "usps_custom_package";
+  //       break;
+  //   }
 
-    switch ($service->carrierCode) {
-      case "dhl":
-        $type =  "dhl_custom_package";
-        break;
-      case "fedex":
-        $type = "fedex_custom_package";
-        break;
-      case "usps":
-        $type = "usps_custom_package";
-        break;
-    }
-
-    return $type;
-  }
+  //   return $type;
+  // }
 }
