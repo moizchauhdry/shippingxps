1f354140bdde0251617f9b47ad8b22fb664240b3
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-01-12 00:08:10 +0500

    order payment notification

--- app/Console/Commands/AnnouncementCommand.php
+++ app/Console/Commands/AnnouncementCommand.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Order;
 use App\Models\User;
 use App\Notifications\AnnouncementNotification;
+use App\Notifications\Shopping\StatusNotification;
 use Illuminate\Console\Command;
 
 class AnnouncementCommand extends Command
@@ -39,11 +41,19 @@ class AnnouncementCommand extends Command
      */
     public function handle()
     {
-        $users = User::where('type', 'customer')->get();
-        foreach ($users as $key => $user) {
-            $user = User::where('email', $user->email)->first();
-            $notification = new AnnouncementNotification();
-            $user->notify($notification);
+        // $users = User::where('type', 'customer')->get();
+        // foreach ($users as $key => $user) {
+        //     $user = User::where('email', $user->email)->first();
+        //     $notification = new AnnouncementNotification();
+        //     $user->notify($notification);
+        //     dump($key);
+        // }
+
+
+        $orders = Order::where('status', 'approved')->where('payment_status','Pending')->get();
+        foreach ($orders as $key => $order) {
+            $customer = User::find($order->customer_id);
+            $customer->notify(new StatusNotification('approved', $order));
             dump($key);
         }
 
