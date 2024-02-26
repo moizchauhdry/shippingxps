0597a82c0e3d296a8e5c49f90d29f957759b3f18
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-02-26 18:16:27 +0500

    # insurance service is now available for all type of packages (single, consolidation and multi-piece)

--- resources/js/Pages/Packages/Components/ServiceComponent.vue
+++ resources/js/Pages/Packages/Components/ServiceComponent.vue
@@ -7,12 +7,8 @@
 				</div>
 				<div class="card-body">
 					<div class="row">
-						<template
-							v-if="
-								$page.props.auth.user.type == 'customer' &&
-								(packag.status == 'open' || packag.status == 'filled')
-							"
-						>
+						<template v-if="$page.props.auth.user.type == 'customer' &&
+							(packag.status == 'open' || packag.status == 'filled')">
 							<div class="col-md-7">
 								<table class="table table-striped">
 									<thead>
@@ -30,41 +26,22 @@
 												<td>{{ service.description }}</td>
 												<td>$ {{ service.price }}</td>
 												<td>
-													<button
-														type="button"
-														v-on:click="setActiveService(service)"
-														class="btn btn-primary btn-sm"
-														v-if="
-															!this.service_requests_service_ids.includes(
-																service.id
-															)
-														"
-													>
-														Request
-													</button>
-													<span class="badge badge-success" v-else>
-														Request Sent
-													</span>
+													<button type="button" v-on:click="setActiveService(service)"
+														class="btn btn-primary btn-sm" v-if="!this.service_requests_service_ids.includes(
+															service.id)">Request</button>
+													<span class="badge badge-success" v-else>Request Sent</span>
 												</td>
 											</template>
 											<template v-else>
-												<template v-if="service.title == 'Trash'">
+												<template
+													v-if="service.title == 'Trash' || service.title == 'Insurance'">
 													<td>{{ service.title }}</td>
 													<td>{{ service.description }}</td>
 													<td>$ {{ service.price }}</td>
 													<td>
-														<button
-															type="button"
-															v-on:click="setActiveService(service)"
-															class="btn btn-primary btn-sm"
-															v-if="
-																!this.service_requests_service_ids.includes(
-																	service.id
-																)
-															"
-														>
-															Request
-														</button>
+														<button type="button" v-on:click="setActiveService(service)"
+															class="btn btn-primary btn-sm" v-if="!this.service_requests_service_ids.includes(
+																service.id)">Request</button>
 														<span class="badge badge-success" v-else>
 															Request Sent
 														</span>
@@ -80,16 +57,9 @@
 									<form @submit.prevent="submit">
 										<div class="form-group">
 											<breeze-label for="notes" value="Message for Admin" />
-											<textarea
-												v-model="form.customer_message"
-												name="notes"
-												id="notes"
-												class="form-control"
-												placeholder="Message for Admin"
-												rows="2"
-												style="resize: none"
-												required
-											>
+											<textarea v-model="form.customer_message" name="notes" id="notes"
+												class="form-control" placeholder="Message for Admin" rows="2"
+												style="resize: none" required>
 											</textarea>
 										</div>
 										<p style="color: red">
@@ -108,31 +78,20 @@
 											Charges : <strong>${{ form.service.price }}</strong>
 										</p>
 										<div class="order-button">
-											<a
-												class="btn btn-danger"
-												v-on:click="cancelServiceForm()"
-											>
-												Cancel</a
-											>
-											<input
-												type="submit"
-												value="Make Request"
-												class="btn btn-success float-right"
-											/>
+											<a class="btn btn-danger" v-on:click="cancelServiceForm()">
+												Cancel</a>
+											<input type="submit" value="Make Request" class="btn btn-success float-right" />
 										</div>
 									</form>
 								</template>
 							</div>
 						</template>
-						<div
-							v-bind:class="{
-								'col-md-8':
-									$page.props.auth.user.type == 'admin' ||
-									$page.props.auth.user.type == 'manager',
-								'col-md-12': $page.props.auth.user.type == 'customer',
-							}"
-							v-if="service_requests.length > 0"
-						>
+						<div v-bind:class="{
+							'col-md-8':
+								$page.props.auth.user.type == 'admin' ||
+								$page.props.auth.user.type == 'manager',
+							'col-md-12': $page.props.auth.user.type == 'customer',
+						}" v-if="service_requests.length > 0">
 							<table class="table table-striped">
 								<thead>
 									<tr>
@@ -140,23 +99,17 @@
 										<template v-if="$page.props.auth.user.type == 'customer'">
 											<th scope="col">Your Message</th>
 										</template>
-										<template
-											v-if="
-												$page.props.auth.user.type == 'admin' ||
-												$page.props.auth.user.type == 'manager'
-											"
-										>
+										<template v-if="$page.props.auth.user.type == 'admin' ||
+											$page.props.auth.user.type == 'manager'
+											">
 											<th scope="col">Customer Message</th>
 										</template>
 										<th scope="col">Admin Response</th>
 										<th scope="col">Status</th>
 										<th scope="col">Charges</th>
-										<template
-											v-if="
-												$page.props.auth.user.type == 'admin' ||
-												$page.props.auth.user.type == 'manager'
-											"
-										>
+										<template v-if="$page.props.auth.user.type == 'admin' ||
+											$page.props.auth.user.type == 'manager'
+											">
 											<th scope="col"></th>
 										</template>
 									</tr>
@@ -173,27 +126,17 @@
 											{{ request.admin_message }}
 										</td>
 										<td>
-											<span
-												v-bind:class="getLabelClass(request.status)"
-												style="padding: 5px"
-											>
+											<span v-bind:class="getLabelClass(request.status)" style="padding: 5px">
 												{{ request.status }}
 											</span>
 										</td>
 										<td>$ {{ request.price }}</td>
 										<td>
-											<template
-												v-if="
-													($page.props.auth.user.type == 'admin' ||
-														$page.props.auth.user.type == 'manager') &&
-													request.status == 'pending'
-												"
-											>
-												<a
-													v-on:click="setServiceResponse(request)"
-													class="link-primary"
-													>Respond</a
-												>
+											<template v-if="($page.props.auth.user.type == 'admin' ||
+												$page.props.auth.user.type == 'manager') &&
+												request.status == 'pending'
+												">
+												<a v-on:click="setServiceResponse(request)" class="link-primary">Respond</a>
 											</template>
 										</td>
 									</tr>
@@ -203,13 +146,9 @@
 						<div v-if="service_requests == 0">
 							<span>There are no services requests added.</span>
 						</div>
-						<div
-							v-if="
-								$page.props.auth.user.type == 'admin' ||
-								$page.props.auth.user.type == 'manager'
-							"
-							class="col-md-4"
-						>
+						<div v-if="$page.props.auth.user.type == 'admin' ||
+							$page.props.auth.user.type == 'manager'
+							" class="col-md-4">
 							<template v-if="form_respond.request != null">
 								<h3>Handle Service Request</h3>
 								<form @submit.prevent="submitRespondForm">
@@ -219,16 +158,9 @@
 									</p>
 									<div class="form-group">
 										<p>Message for Customer</p>
-										<textarea
-											v-model="form_respond.admin_message"
-											name="admin_message"
-											id="admin_message"
-											class="form-control"
-											placeholder="Message for Customer"
-											rows="4"
-											style="resize: none"
-											required
-										>
+										<textarea v-model="form_respond.admin_message" name="admin_message"
+											id="admin_message" class="form-control" placeholder="Message for Customer"
+											rows="4" style="resize: none" required>
 										</textarea>
 									</div>
 									<p style="">
@@ -240,25 +172,15 @@
 										<input type="text" v-model="form_respond.request.price" />
 									</div>
 									<div class="order-button">
-										<input
-											style="display: none"
-											type="submit"
-											value="Update Request"
-											class="btn btn-danger"
-										/>
+										<input style="display: none" type="submit" value="Update Request"
+											class="btn btn-danger" />
 
-										<a
-											v-on:click="requestComplete()"
-											class="btn btn-success float-left"
-										>
+										<a v-on:click="requestComplete()" class="btn btn-success float-left">
 											<span>Complete Request</span>
 										</a>
 
-										<a
-											v-on:click="requestReject()"
-											class="btn btn-danger float-right"
-											style="margin-right: 5px"
-										>
+										<a v-on:click="requestReject()" class="btn btn-danger float-right"
+											style="margin-right: 5px">
 											<span>Reject Request</span>
 										</a>
 									</div>
@@ -273,95 +195,95 @@
 </template>
 
 <script>
-	export default {
-		name: "Service Component",
-		props: {
-			packag: Object,
-			service_requests: Object,
-			service_requests_service_ids: Object,
-			services: Object,
+export default {
+	name: "Service Component",
+	props: {
+		packag: Object,
+		service_requests: Object,
+		service_requests_service_ids: Object,
+		services: Object,
+	},
+	data() {
+		return {
+			form: this.$inertia.form({
+				package_id: this.packag.id,
+				service_id: null,
+				customer_message: "",
+				service: null,
+			}),
+			form_respond: this.$inertia.form({
+				admin_message: "",
+				status: "pending",
+				request: null,
+			}),
+		};
+	},
+	methods: {
+		setActiveService(service) {
+			this.form.service_id = service.id;
+			this.form.service = service;
 		},
-		data() {
-			return {
-				form: this.$inertia.form({
-					package_id: this.packag.id,
-					service_id: null,
-					customer_message: "",
-					service: null,
-				}),
-				form_respond: this.$inertia.form({
-					admin_message: "",
-					status: "pending",
-					request: null,
-				}),
-			};
+		cancelServiceForm() {
+			this.form.service_id = null;
 		},
-		methods: {
-			setActiveService(service) {
-				this.form.service_id = service.id;
-				this.form.service = service;
-			},
-			cancelServiceForm() {
-				this.form.service_id = null;
-			},
-			submitRespondForm() {
-				this.form_respond.post(this.route("packages.service-handle"));
-				this.form_respond.reset();
-				// Inertia.reload({only: ['service_requests']});
-			},
-			setServiceResponse(request) {
-				this.form_respond.request_id = request.id;
-				this.form_respond.request = request;
-			},
-			requestComplete() {
-				this.form_respond.status = "served";
-				this.submitRespondForm();
-			},
-			requestReject() {
-				this.form_respond.status = "rejected";
-				this.submitRespondForm();
-			},
-			submit() {
-				this.form.post(this.route("packages.service-request"));
-				this.form.reset();
-				Inertia.reload({ only: ["service_requests"] });
-			},
-			getLabelClass(status) {
-				switch (status) {
-					case "pending":
-						return "text-uppercase badge badge-warning p-2 text-white";
-						break;
-					case "open":
-						return "text-uppercase badge badge-info p-2 text-white";
-						break;
-					case "filled":
-						return "text-uppercase badge badge-info p-2 text-white";
-						break;
-					case "open":
-						return "text-uppercase badge badge-success p-2 text-white";
-						break;
-					case "labeled":
-						return "text-uppercase badge badge-success p-2 text-white";
-						break;
-					case "shipped":
-						return "text-uppercase badge badge-primary p-2";
-						break;
-					case "delivered":
-						return "text-uppercase badge badge-success p-2 text-white";
-						break;
-					case "consolidation":
-						return "text-uppercase badge badge-danger p-2 text-white";
-						break;
-					case "served":
-						return "label bg-success";
-						break;
-					case "rejected":
-						return "label bg-danger";
-						break;
-					default:
-						return "text-uppercase badge badge-primary p-2 text-white";
-				}
-			},
+		submitRespondForm() {
+			this.form_respond.post(this.route("packages.service-handle"));
+			this.form_respond.reset();
+			// Inertia.reload({only: ['service_requests']});
 		},
-	};
+		setServiceResponse(request) {
+			this.form_respond.request_id = request.id;
+			this.form_respond.request = request;
+		},
+		requestComplete() {
+			this.form_respond.status = "served";
+			this.submitRespondForm();
+		},
+		requestReject() {
+			this.form_respond.status = "rejected";
+			this.submitRespondForm();
+		},
+		submit() {
+			this.form.post(this.route("packages.service-request"));
+			this.form.reset();
+			Inertia.reload({ only: ["service_requests"] });
+		},
+		getLabelClass(status) {
+			switch (status) {
+				case "pending":
+					return "text-uppercase badge badge-warning p-2 text-white";
+					break;
+				case "open":
+					return "text-uppercase badge badge-info p-2 text-white";
+					break;
+				case "filled":
+					return "text-uppercase badge badge-info p-2 text-white";
+					break;
+				case "open":
+					return "text-uppercase badge badge-success p-2 text-white";
+					break;
+				case "labeled":
+					return "text-uppercase badge badge-success p-2 text-white";
+					break;
+				case "shipped":
+					return "text-uppercase badge badge-primary p-2";
+					break;
+				case "delivered":
+					return "text-uppercase badge badge-success p-2 text-white";
+					break;
+				case "consolidation":
+					return "text-uppercase badge badge-danger p-2 text-white";
+					break;
+				case "served":
+					return "label bg-success";
+					break;
+				case "rejected":
+					return "label bg-danger";
+					break;
+				default:
+					return "text-uppercase badge badge-primary p-2 text-white";
+			}
+		},
+	},
+};
 </script>
