d65c2736c03d4251e8f0148acbbdc9dd8e4e4c32
noaman-hashmi <noaman@skynetsolutionz.com>
2024-06-01 23:48:02 +0500

    mage report allotment

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,73 +4,89 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Package;
+use App\Models\Payment;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
     public function index(Request $request)
     {
-        $suit_no = intval($request->suit_no) - 4000;
+        $user = Auth::user();
+        $search_invoice_no = $request->search_invoice_no;
+        $search_suit_no = $request->search_suit_no;
+        $search_tracking_out = $request->search_tracking_out;
 
-        $query = Package::with('customer', 'warehouse', 'child_packages', 'boxes')
-            ->when(Auth::user()->type == 'customer', function ($qry) {
-                $qry->where('customer_id', Auth::user()->id);
-            })
-            ->when($request->pkg_id && !empty($request->pkg_id), function ($qry) use ($request) {
-                $qry->where('id', $request->pkg_id);
-            })
-            ->when($request->suit_no && !empty($request->suit_no), function ($qry) use ($suit_no) {
-                $qry->where('customer_id', $suit_no);
-            })
-            ->when($request->pkg_type && !empty($request->pkg_type), function ($qry) use ($request) {
-                $qry->where('pkg_type', $request->pkg_type);
-            })
-            ->when($request->pkg_carrier && !empty($request->pkg_carrier), function ($qry) use ($request) {
-                $qry->where('carrier_code', $request->pkg_carrier);
-            })
-            ->when($request->pkg_status && !empty($request->pkg_status), function ($qry) use ($request) {
-                if ($request->pkg_status == 'shipped') {
-                    $qry->where('tracking_number_out', '!=', NULL);
-                } else {
-                    $qry->where('status', $request->pkg_status);
-                }
-            })
-            ->when($request->payment_status && !empty($request->payment_status), function ($qry) use ($request) {
-                $qry->where('payment_status', $request->payment_status);
-            })
-            ->when($request->tracking_out && !empty($request->tracking_out), function ($qry) use ($request) {
-                $qry->where('tracking_number_out', $request->tracking_out);
-            })
-            ->when($request->auctioned && !empty($request->auctioned), function ($qry) use ($request) {
-                $qry->where('auctioned', $request->auctioned);
-            })
-            ->when($request->date_range && !empty($request->date_range), function ($qry) use ($request) {
-                $range = explode(' - ', $request->date_range);
-                $from = date("Y-m-d", strtotime($range[0]));
-                $to = date("Y-m-d", strtotime($range[1]));
-                $qry->whereDate('created_at', '>=', $from)->whereDate('created_at', '<=', $to);
-            });
+        $query = Payment::query();
 
-        $packages_count = $query->count();
-        $packages = $query->orderBy('id', 'desc')->paginate(10)->withQueryString();
+        $query->select(
+            'u.id as u_id',
+            'u.name as u_name',
+            'payments.id as p_id',
+            'payments.transaction_id as t_id',
+            'payments.payment_type as p_method',
+            'payments.charged_amount as charged_amount',
+            'payments.charged_at as charged_at',
+            'pkg.id as pkg_id',
+            'pkg.service_label as pkg_service_label',
+            'pkg.tracking_number_out as pkg_tracking_out',
+            'pkg.shipping_charges_gross',
+            'pkg.shipping_markup_percentage',
+            'pkg.shipping_markup_fee',
+            DB::raw('CASE 
+                WHEN payments.package_id IS NOT NULL THEN "package"
+                WHEN payments.order_id IS NOT NULL THEN "order"
+                WHEN payments.gift_card_id IS NOT NULL THEN "gift"
+                ELSE "unknown"
+            END AS p_type'),
+            DB::raw('CASE 
+                WHEN payments.package_id IS NOT NULL THEN payments.package_id
+                WHEN payments.order_id IS NOT NULL THEN payments.order_id
+                WHEN payments.gift_card_id IS NOT NULL THEN payments.gift_card_id
+                ELSE "unknown"
+            END AS p_type_id')
+        );
 
-        $open_pkgs_count = Package::where('status', 'open')->where('pkg_type', 'single')->count();
+        $query->join('users as u', 'u.id', 'payments.customer_id');
+        $query->leftJoin('packages as pkg', 'pkg.id', 'payments.package_id');
+
+        $query->where('payments.charged_at', '!=', NULL);
+        
+        $query->when($user->type === 'customer', function ($qry) use ($user) {
+            $qry->where('payments.customer_id', $user->id);
+        });
+
+        $query->when($search_invoice_no && !empty($search_invoice_no), function ($qry) use ($search_invoice_no) {
+            $qry->where('payments.id', $search_invoice_no);
+        });
+
+        $query->when($search_tracking_out && !empty($search_tracking_out), function ($qry) use ($search_tracking_out) {
+            $qry->where('pkg.tracking_number_out', $search_tracking_out);
+        });
+
+        $query->when($search_suit_no && !empty($search_suit_no), function ($qry) use ($search_suit_no) {
+            $suit_no = (int) $search_suit_no;
+            $suit_no = $suit_no - 4000;
+            $qry->where('u.id', $suit_no);
+        });
+
+        $query->when($request->date_range && !empty($request->date_range), function ($qry) use ($request) {
+            $range = explode(' - ', $request->date_range);
+            $from = date("Y-m-d", strtotime($range[0]));
+            $to = date("Y-m-d", strtotime($range[1]));
+            $qry->whereDate('charged_at', '>=', $from)->whereDate('charged_at', '<=', $to);
+        });
+
+        $payments = $query->orderBy('payments.id', 'desc')->paginate(10)->withQueryString();
 
         return Inertia::render('Reports/PackageReport', [
-            'pkgs' => $packages,
-            'open_pkgs_count' => $open_pkgs_count,
-            'packages_count' => $packages_count,
+            'payments' => $payments,
             'filters' => [
-                'pkg_id' => $request->pkg_id ?? "",
-                'suit_no' => $request->suit_no ?? "",
-                'pkg_status' => $request->pkg_status ?? "",
-                'pkg_type' => $request->pkg_type ?? "",
-                'pkg_carrier' => $request->pkg_carrier ?? "",
-                'payment_status' => $request->payment_status ?? "",
-                'auctioned' => $request->auctioned ?? "",
+                'search_invoice_no' => $search_invoice_no ?? "",
+                'search_suit_no' => $search_suit_no ?? "",
+                'search_tracking_out' => $request->search_tracking_out ?? "",
                 'date_range' => $request->date_range ?? "",
-                'tracking_out' => $request->tracking_out ?? "",
             ]
         ]);
     }
--- resources/js/Pages/Reports/PackageReport.vue
+++ resources/js/Pages/Reports/PackageReport.vue
@@ -1,95 +1,96 @@
 <template>
 	<MainLayout>
 		<div class="card mb-5">
-			<div class="card-header">
-				<b>Manage Report</b>
-				
-			</div>
+			<div class="card-header">Manage Reports</div>
 			<div class="card-body">
-				<div class="row">
-					<div class="col-md-12">
-						<form @submit.prevent="submit">
-							<div class="d-flex search">
-								<div class="form-group">
-									<label for="">Package Number</label>
-									<input type="number" name="number" v-model="form.pkg_id" class="form-control" />
-								</div>
-								<div class="form-group">
-									<label for="">Suit Number</label>
-									<input type="number" name="number" v-model="form.suit_no" class="form-control" />
-								</div>
-								<div class="form-group">
-									<label for="">Tracking Out</label>
-									<input type="text" v-model="form.tracking_out" class="form-control" />
-								</div>
-								<div class="form-group">
-									<label for="">Package Status</label>
-									<select class="form-control custom-select" v-model="form.pkg_status">
-										<option value="" selected>All</option>
-										<option value="open">Open</option>
-										<option value="filled">Filled</option>
-										<option value="checkout">Checkout</option>
-										<option value="shipped">Shipped</option>
-										<option value="rejected">Rejected</option>
-									</select>
-								</div>
-								<div class="form-group">
-									<label for="">Package Type</label>
-									<select class="form-control custom-select" v-model="form.pkg_type">
-										<option value="" selected>All</option>
-										<option value="single">Single</option>
-										<option value="consolidation">Consolidation</option>
-										<option value="multipiece">Multipiece</option>
-										<option value="assigned">Assigned</option>
-									</select>
-								</div>
-								<div class="form-group">
-									<label for="">Payment Status</label>
-									<select class="form-control custom-select" v-model="form.payment_status">
-										<option value="" selected>All</option>
-										<option value="Paid">Paid</option>
-										<option value="Pending">Pending</option>
-									</select>
-								</div>
-								<div class="form-group">
-									<label for="">Auction Status</label>
-									<select class="form-control custom-select" v-model="form.auctioned">
-										<option value="" selected>All</option>
-										<option value="1">Yes</option>
-										<option value="0">No</option>
-									</select>
-								</div>
-								<div class="form-group">
-									<label for="">Shipping Service</label>
-									<select class="form-control custom-select" v-model="form.pkg_carrier">
-										<option value="" selected>All</option>
-										<option value="fedex">FEDEX</option>
-										<option value="ups">UPS</option>
-										<option value="dhl">DHL</option>
-										<option value="usps">USPS</option>
-									</select>
-								</div>
-								<div class="form-group">
-									<label for="">Date Range</label>
-									<Datepicker v-model="date" range :format="format" :enableTimePicker="false">
-									</Datepicker>
-								</div>
-							</div>
-							<div class="row">
-								<div class="form-group col-md-12">
-									<button type="submit" class="btn btn-primary mr-1">Search</button>
-									<button type="button" class="btn btn-info" @click="clear()">Clear</button>
-								</div>
-							</div>
-						</form>
+				<form @submit.prevent="submit">
+					<div class="d-flex search">
+						<div class="form-group">
+							<label for="">Invoice No</label>
+							<input type="text" class="form-control" v-model="form.search_invoice_no" />
+						</div>
+						<div class="form-group">
+							<label for="">Suit No</label>
+							<input type="text" class="form-control" v-model="form.search_suit_no" />
+						</div>
+						<div class="form-group">
+							<label for="">Tracking Out</label>
+							<input type="text" class="form-control" v-model="form.search_tracking_out" />
+						</div>
+						<div class="form-group">
+							<label for="">Date Range</label>
+							<Datepicker v-model="date" range :format="format" :enableTimePicker="false"></Datepicker>
+						</div>
 					</div>
-				</div>
+					<div class="row">
+						<div class="form-group col-md-12">
+							<button type="submit" class="btn btn-primary mr-1">Search</button>
+							<button type="button" class="btn btn-info" @click="clear()">Clear</button>
+						</div>
+					</div>
+				</form>
 
-				<package-list-component v-bind="$props"></package-list-component>
+				<div class="table-responsive">
+					<table class="table table-striped table-bordered text-uppercase">
+						<thead>
+							<tr>
+								<th>Invoive ID</th>
+								<th>Customer</th>
+								<th>Payment Type</th>
+								<th>Transaction ID</th>
+								<th>Charged Date</th>
+								<th>charged Amount</th>
+								<th>Shipping Total</th>
+								<th>Markup Percentage</th>
+								<th>Markup / Profit</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="(payment, index) in payments.data" :key="payment.id">
+								<td>{{ payment.p_id }}</td>
+								<td>{{ payment.u_name }} - {{ siuteNum(payment.u_id) }}</td>
+								<td>
+									<template v-if="payment.p_type === 'package'">
+										<inertia-link :href="route('packages.show', payment.p_type_id)">
+											<span class="font-bold text-primary underline">{{ payment.p_type }} - {{
+												payment.p_type_id }}</span>
+										</inertia-link>
+										<br>
+										{{ payment.pkg_service_label }}
+										<br>
+										{{ payment.pkg_tracking_out }}
+									</template>
+
+
+									<template v-if="payment.p_type === 'order'">
+										{{ payment.p_type }} - {{ payment.p_type_id }}
+									</template>
+									<inertia-link v-if="payment.p_type === 'gift'"
+										:href="route('gift-card.edit', payment.p_type_id)">
+										{{ payment.p_type }} - {{ payment.p_type_id }}
+									</inertia-link>
+								</td>
+								<td>
+									{{ payment.t_id }} <br>
+									{{ payment.p_method }}
+								</td>
+								<td>{{ payment.charged_at }}</td>
+								<td>${{ payment.charged_amount }}</td>
+								<td>${{ payment.shipping_charges_gross }}</td>
+								<td>{{ payment.shipping_markup_percentage }}%</td>
+								<td>${{ payment.shipping_markup_fee }}</td>
+							</tr>
+							<tr v-if="payments.data.length == 0">
+								<td class="text-primary text-center" colspan="9">
+									There are no payments found.
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
 			<div class="card-footer">
-				<span class="float-left"><b>Showing Records: {{ packages_count }}</b></span>
-				<pagination :links="pkgs.links" class="float-right"></pagination>
+				<pagination :links="payments.links" class="float-right"></pagination>
 			</div>
 		</div>
 	</MainLayout>
@@ -98,65 +99,40 @@
 <script>
 import MainLayout from "@/Layouts/Main";
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated";
-import { Inertia } from "@inertiajs/inertia";
-import PackageListComponent from "../Reports/Components/PackageListComponent.vue";
+import BreezeLabel from "@/Components/Label";
+import Paginate from "@/Components/Paginate";
 import Datepicker from "vue3-date-time-picker";
 import "vue3-date-time-picker/dist/main.css";
 import Pagination from "@/Components/Pagination.vue";
+import { Inertia } from "@inertiajs/inertia";
 
 export default {
+	data() {
+		return {
+			form: {
+				search_invoice_no: this.filters.search_invoice_no,
+				search_suit_no: this.filters.search_suit_no,
+				search_tracking_out: this.filters.search_tracking_out,
+				date_range: this.filters.date_range,
+			},
+			date: "",
+		};
+	},
 	components: {
 		BreezeAuthenticatedLayout,
 		MainLayout,
-		PackageListComponent,
+		BreezeLabel,
+		Paginate,
 		Datepicker,
 		Pagination
 	},
 	props: {
 		auth: Object,
-		pkgs: Object,
-		open_pkgs_count: Object,
-		packages_count: Object,
+		payments: Object,
 		filters: Object,
 	},
-	data() {
-		return {
-			active: "packages",
-			date: "",
-			form: {
-				pkg_id: this.filters.pkg_id,
-				suit_no: this.filters.suit_no,
-				pkg_status: this.filters.pkg_status,
-				pkg_type: this.filters.pkg_type,
-				pkg_carrier: this.filters.pkg_carrier,
-				payment_status: this.filters.payment_status,
-				auctioned: this.filters.auctioned,
-				tracking_out: this.filters.tracking_out,
-				date_range: this.filters.date_range,
-			},
-		};
-	},
+	mounted() { },
 	methods: {
-		submit() {
-			const queryParams = new URLSearchParams(this.form);
-			const url = `${route("report.index")}?${queryParams.toString()}`;
-			Inertia.visit(url, { preserveState: true });
-		},
-		siuteNum(user_id) {
-			return 4000 + user_id;
-		},
-		clear() {
-			this.form.pkg_id = "";
-			this.form.suit_no = "";
-			this.form.pkg_status = "";
-			this.form.pkg_type = "";
-			this.form.pkg_carrier = "";
-			this.form.payment_status = "";
-			this.form.date_range = "";
-			this.form.tracking_out = "";
-			this.date = "";
-			this.submit();
-		},
 		format() {
 			var start = new Date(this.date[0]);
 			var end = new Date(this.date[1]);
@@ -170,6 +146,22 @@ export default {
 			this.form.date_range = `${startYear}/${startMonth}/${startDay} - ${endYear}/${endMonth}/${endDay}`;
 			return `${startDay}/${startMonth}/${startYear} - ${endDay}/${endMonth}/${endYear}`;
 		},
+		submit() {
+			const queryParams = new URLSearchParams(this.form);
+			const url = `${route("report.index")}?${queryParams.toString()}`;
+			Inertia.visit(url, { preserveState: true });
+		},
+		siuteNum(user_id) {
+			return 4000 + user_id;
+		},
+		clear() {
+			this.form = {};
+			this.date = "";
+			this.submit();
+		},
+	},
+	created() {
+		console.log(this.data);
 	},
 };
 </script>
