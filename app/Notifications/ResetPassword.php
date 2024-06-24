5757e140483bd823f5e5af647d8167af5e72e2a1
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-06-25 00:32:27 +0500

    shipping xps custom reset password email notification

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPassword extends Notification
+{
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = route('password.reset', $this->token);
+
+        return (new MailMessage)
+            ->subject('Important Notification')
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', url($url))
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+}
