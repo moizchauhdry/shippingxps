fb225d9f548c40e90f2563d8263e78624c39f303
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-10-22 17:16:15 +0500

    storage policy

--- resources/js/Pages/Packages/Components/StorageComponent.vue
+++ resources/js/Pages/Packages/Components/StorageComponent.vue
@@ -9,9 +9,28 @@
             Days Exceeded: {{ packag.storage_days_exceeded }} <br>
             Storage Charges: ${{ packag.storage_fee }} <br>
             <h6 class="mt-3">
-                Note: The storage fee is added when a package exceeds 75 days, and after 80 days, the package is terminated
+                Note: The storage fee is added when a package exceeds 75 days, and after 80 days, the package is
+                terminated
                 and becomes the property of ShippingXPS.
             </h6>
+
+            <br>
+
+            <h1 class="text-danger text-lg">Effective December 1, 2024, the storage policy will be updated as follows: </h1>
+            <ul class="">
+                <li>
+                    * Storage days will change from 75 days to 30 days.
+                </li>
+                <li>
+                    * A storage fee will apply to packages held for more than 30 days.
+                </li>
+                <li>
+                    * Packages remaining beyond 35 days will be terminated and become the property of ShippingXPS.
+                </li>
+                <li>
+                    * For the five-day period between 30 and 35 days, a daily fee will be charged.
+                </li>
+            </ul>
         </div>
     </div>
 </template>
