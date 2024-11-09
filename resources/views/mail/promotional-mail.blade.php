b5b5b867ae6f2c48d3055a73c345e27b6f3c7184
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-11-10 03:59:52 +0500

    promotional email

--- resources/views/mail/promotional-mail.blade.php
+++ resources/views/mail/promotional-mail.blade.php
@@ -1,14 +1,23 @@
-@extends('email')
+@extends('email-2')
 
 @section('content')
-<div style="margin-top:50px">
-    <a href="https://selfshiplabel.com" target="_blank">
+<div style="margin-top:50px; text-align:center">
+    {{-- <a href="https://selfshiplabel.com" target="_blank">
         <h1>https://selfshiplabel.com</h1>
-    </a> <br>
+    </a> <br> --}}
 
     <a href="https://selfshiplabel.com" target="_blank">
         <img src="https://app.shippingxps.com/images/promotional-banner.png" alt="" class="responsive-img">
     </a>
+
+    <p>
+        Thank you for using our application! <br>
+    </p>
+
+    <p>
+        ©{{Carbon\Carbon::now()->year }} <a href="http://shippingxps.com">ShippingXPS.com</a> <br>
+        <span>All Rights Reserved</span>
+    </p>
 </div>
 
 <style>
