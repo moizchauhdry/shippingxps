baf428749efdf3fb873fc1ae7a3fbf8911fbee46
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2024-12-21 21:03:14 +0500

    email verification and route corrections

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use OwenIt\Auditing\Contracts\Auditable;
 
-class User extends Authenticatable implements Auditable, MustVerifyEmail
+class User extends Authenticatable implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
     use HasFactory, Notifiable, HasApiTokens;
